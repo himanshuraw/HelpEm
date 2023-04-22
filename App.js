9057aac73b3560fc3cbe8cd3b95723770b987ada
Himanshu Rawat <himanshurawat180@gmail.com>
2023-04-22 15:48:25 +0530

    Used ToastAndroid

--- App.js
+++ App.js
@@ -3,23 +3,25 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import AdminLogin from "./screens/AdminLogin";
 import FindScreen from "./screens/FindScreen";
 import FoundScreen from "./screens/FoundScreen";
-import HelpScreen from "./screens/HelpScreen";
 import HomeScreen from "./screens/HomeScreen";
 import RegisterVictim from "./screens/RegisterVictim";
+import { AuthProvider } from "./context/AuthContext";
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
+    <AuthProvider>
+
     <NavigationContainer>
       <Stack.Navigator>
         <Stack.Screen name="Home" component={HomeScreen} />
         <Stack.Screen name="Find" component={FindScreen} />
-        <Stack.Screen name="Help" component={HelpScreen} />
         <Stack.Screen name="Found" component={FoundScreen} />
         <Stack.Screen name="AdminLogin" component={AdminLogin} />
         <Stack.Screen name="RegisterVictim" component={RegisterVictim} />
       </Stack.Navigator>
     </NavigationContainer>
+    </AuthProvider>
   );
 }
